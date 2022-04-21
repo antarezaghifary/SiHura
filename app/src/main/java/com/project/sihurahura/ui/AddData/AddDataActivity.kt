7309d4ff35f13949bdb70a0caff429d5d6408cb7
Antareza Ghifary <oneakun08@gmail.com>
2022-04-21 14:36:07 +0700

    add fullscreen in AddDataActivity.kt

--- app/src/main/java/com/project/sihurahura/ui/AddData/AddDataActivity.kt
+++ app/src/main/java/com/project/sihurahura/ui/AddData/AddDataActivity.kt
@@ -1,12 +1,23 @@
 package com.project.sihurahura.ui.AddData
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.project.sihurahura.R
+import com.oratakashi.viewbinding.core.binding.activity.viewBinding
+import com.project.sihurahura.databinding.ActivityAddDataBinding
 
 class AddDataActivity : AppCompatActivity() {
+    private val binding: ActivityAddDataBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_data)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        with(binding) {
+
+        }
     }
 }
